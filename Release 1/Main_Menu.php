40fffc2cea2a74d52f05b50c0f3a2354b6cdd073
peer <wpcvail@gmail.com>
2020-05-12 20:48:03 -0700

    Update Main_Menu.php

--- Release 1/Main_Menu.php
+++ Release 1/Main_Menu.php
@@ -38,10 +38,12 @@
 
 	echo "<p>Welcome to SPLER Education, ".$query_array["user"]."</p>";
 	echo"</pre>";
-
+?>
 
 <div class="button">
-	<?php echo '<a  href="../play_game.php/?user='.$query_array["user"].'" >Play Game</a>' ?>
+	<?php
+	 echo '<a  href="../play_game.php/?user='.$query_array["user"].'" >Play Game</a>' 
+	 ?>
 <div>
 
 </body>
